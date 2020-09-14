2063a57fb9f492a2a427031e64b05ed4a1000dee
dev_dastle <luis_castle_2011@hotmai.com>
2020-09-14 15:18:35 -0500

    Seccion 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,60 @@
-import { SERVER_PORT } from './../global/environment';
+import * as socket from '../sockets/socket';
+
+import { SERVER_PORT } from '../global/environment';
 import express from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
 
 export default class Server {
+
+    private static _intance: Server;
+
     public app: express.Application;
     public port: number;
 
-    constructor() {
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+
+    private constructor() {
+
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server( this.app );
+        this.io = socketIO( this.httpServer );
+
+        this.escucharSockets();
     }
 
-    start(callback: Function) {
-        this.app.listen(this.port, callback);
+    public static get instance() {
+        return this._intance || ( this._intance = new this() );
     }
+
+
+    private escucharSockets() {
+
+        console.log('Escuchando conexiones - sockets');
+
+        this.io.on('connection', cliente => {
+
+            console.log('Cliente conectado');
+
+            // Mensajes
+            socket.mensaje( cliente, this.io );
+
+            // Desconectar
+            socket.desconectar( cliente );         
+
+        });
+
+    }
+
+
+    start( callback: Function ) {
+
+        this.httpServer.listen( this.port, callback );
+
+    }
+
 }
--- index.ts
+++ index.ts
@@ -1,19 +1,25 @@
-import Server from "./classes/server";
+import Server from './classes/server';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import router from './routes/router';
 
-const server = new Server();
+const server = Server.instance;
 
 // BodyParser
-server.app.use(bodyParser.urlencoded({ extended: true}));
-server.app.use(bodyParser.json());
+server.app.use( bodyParser.urlencoded({ extended: true }) );
+server.app.use( bodyParser.json() );
 
 // CORS
-server.app.use(cors({origin: true, credentials: true}));
+server.app.use( cors({ origin: true, credentials: true  }) );
 
-server.app.use('/', router);
 
-server.start(() => {
+// Rutas de servicios
+server.app.use('/', router );
+
+
+
+
+server.start( () => {
     console.log(`Servidor corriendo en el puerto ${ server.port }`);
 });
+
